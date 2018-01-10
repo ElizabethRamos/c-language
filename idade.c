cd7f459be6b1b8bd8916fc5696340e028c1a05f8
Elizabeth Ramos <elizabethlopesramos@gmail.com>
2018-01-10 18:46:02 -0200

    Declaration of variables

---
+++ idade.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+  //armazena variaveis de idade e ano
+  int idade = 29, ano = 2018;
+  float PI = 3.14;
+
+  printf("Idade: %d\n", idade);
+  printf("Ano: %d\n", ano);
+  printf("PI %f\n", PI);
+
+  return 0;
+}
