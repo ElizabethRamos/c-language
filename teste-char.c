a27ec61a13038070a393a7303b47e500d6a67a21
Elizabeth Ramos <elizabethlopesramos@gmail.com>
2018-01-17 17:03:13 -0200

    Char, int and double tipes

---
+++ teste-char.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    char c = 'E';
+    printf("Letra: %c", c);
+    return 0;
+}
---
+++ teste-inteiro.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int idade1;
+    int idade2;
+
+    idade1 = 29;
+    idade2 = 37;
+
+    printf("%d\t%d", idade1, idade2);
+
+    return 0;
+}
