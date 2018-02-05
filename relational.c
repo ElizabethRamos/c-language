3518515576c97ac13860d49c961c973af35b055f
Elizabeth Ramos <elizabethlopesramos@gmail.com>
2018-02-05 10:17:38 +0200

    Relational operators

---
+++ relational.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+/*
+Relational operators
+> -> bigger then
+< -> less than
+>= -> Bigger or equal
+<= -> less or equal
+=! ->different than
+== -> equal
+In C, 0 returns false and 1 returns true 
+*/
+    int num1 = 20, num2 = 20;
+    printf("%d", num1 == num2);
+    return 0;
+}
