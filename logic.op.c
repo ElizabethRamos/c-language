a4536749c62071385eba2496683d8ef9c0d3a317
Elizabeth Ramos <elizabethlopesramos@gmail.com>
2018-02-05 21:08:44 +0200

    Logic operators

---
+++ logic.op.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    /*
+    Logic Operators
+
+    && -> AND (conjunction)
+    || -> OR (disjunction)
+    ! -> (negation)
+    
+    */
+    
+    int n1 =10, n2 =20; 
+    printf("%d", (n1 < 20) && (n2 >= 20));
+
+    return 0;   
+}
