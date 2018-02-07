05b5528a45b767f94924ebf86b4364de87e1d8db
Elizabeth Ramos <elizabethlopesramos@gmail.com>
2018-02-07 13:10:37 +0200

    Using the comand for to print some message x times

---
+++ for.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+Using the comand for, to repear some phrase.
+*/
+int main(int argc, char *argv[])
+{
+    int time = 1;
+    for(time = 1; time <=50; time++) 
+    {
+        printf("%d - Still training some repetition comand, be patient :)\n", time);
+    }
+    return 0;
+}
