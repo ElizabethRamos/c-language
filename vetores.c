39714a89dee80bf028d571fe6390c4fca8f1c26b
Elizabeth Ramos <elizabethlopesramos@gmail.com>
2018-02-10 11:20:06 +0200

    Use array muktiplication

--- vetores.c
+++ vetores.c
@@ -2,17 +2,19 @@
 
 int main(int argc, char *argv[])
 {
-    int vet[] = {1, 2, 3, 4};
+    int vet[] = {2, 4, 6, 8};
     int i;
 
-    for(i = 0; i < 4; i++)
+    for(i = 3; i >= 0; i--)
     {   
+        printf("Before  i = %d, vet[%d] = %d\n", i, i, vet[i]); 
         vet[i] = vet[i] * 2;
+        printf("After i = %d, vet[%d] = %d\n\n", i, i, vet[i]); 
     }
 
     for(i = 0; i < 4; i++)
     {   
-       printf("Each vet element * 2: %d\n", vet[i]);
+       printf("Each vet element *º 2: %d\n", vet[i]);
     }
 
     return 0;
