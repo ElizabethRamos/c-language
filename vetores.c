e873502911c0b2933e892578ed5ea1cf7e8a690a
Elizabeth Ramos <elizabethlopesramos@gmail.com>
2018-02-10 11:06:45 +0200

    Use array multiplications

---
+++ vetores.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int vet[] = {1, 2, 3, 4};
+    int i;
+
+    for(i = 0; i < 4; i++)
+    {   
+        vet[i] = vet[i] * 2;
+    }
+
+    for(i = 0; i < 4; i++)
+    {   
+       printf("Each vet element * 2: %d\n", vet[i]);
+    }
+
+    return 0;
+}
