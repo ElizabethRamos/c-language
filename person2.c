5fdd55634fd4673ba2fa3c590266db34f0378478
Elizabeth Ramos <elizabethlopesramos@gmail.com>
2018-02-24 16:19:45 +0200

    Use pointer to change variable age

---
+++ person2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct person 
+{
+    char name[100];
+    int age;
+    float kg;
+} t_person;
+
+int main()
+{   
+    t_person person;
+    t_person *p;
+
+    strcpy(person.name, "Eliza Ramos");
+    person.age = 29;
+    person.kg = 70;
+
+    p = &person;
+    p->age = 30;
+
+    printf("Age: %d\n", person.age);
+
+    return 0;
+}
