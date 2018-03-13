d61aea004462ca6446a8cc17cf1059a02ca2d02d
Elizabeth Ramos <elizabethlopesramos@gmail.com>
2018-03-13 16:00:44 +0200

    library functions math.h

---
+++ math.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char const *argv[])
+{
+  printf("%lf\n", exp(2));
+  printf("%lf\n", log10(10));
+  printf("%lf\n", pow(5, 5));
+  printf("%lf\n", sqrt(25));
+  printf("%lf\n", ceil(5.4));
+  printf("%lf\n", floor(5.4));
+  printf("%lf\n", fabs(-10.45));
+
+  return 0;
+}
