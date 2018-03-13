9405c50a3d6c279275a0be017a7575fe231f1c23
Elizabeth Ramos <elizabethlopesramos@gmail.com>
2018-03-13 13:08:26 +0200

    Creates a vector of pointers

---
+++ pointer_vector.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+cria um vetor de apontadores
+*/
+
+int main()
+{
+    const char *languages[] = {"Ruby", "C", "Javascript", "php", "C#"};
+
+    printf("%s\n", languages[0]);
+
+    return 0;
+}
